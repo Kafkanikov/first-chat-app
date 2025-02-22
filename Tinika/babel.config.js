37710dd23b212b1dda504d633a599b90204c208b
Martin <martin.meas5@gmail.com>
2025-02-23 00:09:07 +0700

    Add supabase connection

--- Tinika/babel.config.js
+++ Tinika/babel.config.js
@@ -1,10 +1,19 @@
-module.exports = function(api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo'],
-      plugins: [
-        'babel-plugin-react-native',
-        'react-native-reanimated/plugin',
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "babel-plugin-react-native",
+      "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@": "./",
+          },
+        },
       ],
-    };
+    ],
   };
+};
